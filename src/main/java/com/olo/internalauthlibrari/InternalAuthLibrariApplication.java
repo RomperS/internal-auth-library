d29c53145700a3a1f8e16624a8604a636143f59e
Oswaldo Lopez <77045040+RomperS@users.noreply.github.com>
2025-09-13 18:04:45 -0500

    feat: initial commit

---
+++ src/main/java/com/olo/internalauthlibrari/InternalAuthLibrariApplication.java
@@ -0,0 +1,13 @@
+package com.olo.internalauthlibrari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternalAuthLibrariApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternalAuthLibrariApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/olo/internalauthlibrari/InternalAuthLibrariApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olo.internalauthlibrari;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternalAuthLibrariApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
