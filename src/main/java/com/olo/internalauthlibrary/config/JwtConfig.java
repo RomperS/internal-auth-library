63892fcfa36bc039712bf8239d9729c550f39049
Oswaldo Lopez <77045040+RomperS@users.noreply.github.com>
2025-09-13 20:00:21 -0500

    feat(jwt): Add auto-configuration for JwtService

---
+++ src/main/java/com/olo/internalauthlibrary/config/JwtConfig.java
@@ -0,0 +1,9 @@
+package com.olo.internalauthlibrary.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.olo.internalauthlibrary.security")
+public class JwtConfig {
+}
